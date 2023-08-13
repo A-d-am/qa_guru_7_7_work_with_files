eecf887155db7a273e43ca874b3ca22e203a2ac3
A-d-a-m <adam3928@mail.ru>
2023-08-13 15:18:35 +0200

    added test formalization and asserts

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -1,15 +1,33 @@
 import pypdf
-# TODO оформить в тест, добавить ассерты и использовать универсальный путь
-reader = pypdf.PdfReader('resources/docs-pytest-org-en-latest.pdf')
-number_of_pages = len(reader.pages)
-first_page = reader.pages[0]
-text = first_page.extract_text()
-# print(number_of_pages)
-# print(first_page)
-print(text)
-count = 0
-for image_file in first_page.images:
-    with open(str(count) + image_file.name, 'wb') as fp:
-        fp.write(image_file.data)
-        count += 1
+import os
+from conftest import RESOURCES_DIR
 
+
+def test_pdf():
+    pdf_file_path = os.path.join(RESOURCES_DIR, 'docs-pytest-org-en-latest.pdf')
+    reader = pypdf.PdfReader(pdf_file_path)
+    number_of_pages = len(reader.pages)
+    first_page = reader.pages[0]
+    first_page_text = first_page.extract_text()
+    expected_first_page_text = ('pytest Documentation\nRelease 0.1\nholger krekel, '
+                                'trainer and consultant, https://merlinux.eu/\nJul 14, 2022')
+
+    count = 0
+
+    pdf_file_images_folder = os.path.abspath('pdf_file_images')
+
+    if not os.path.exists(pdf_file_images_folder):
+        os.mkdir('pdf_file_images')
+
+    for image_file in first_page.images:
+        image_file_name = str(count) + image_file.name
+        path_to_image_file = os.path.join(pdf_file_images_folder, image_file_name)
+        with open(path_to_image_file, 'wb') as fp:
+            fp.write(image_file.data)
+            count += 1
+
+    assert number_of_pages == 412, f'Wrong number of pdf document pages: expected {412}, got {number_of_pages}'
+    assert first_page_text == expected_first_page_text, \
+        f'Wrong first page text: expected {expected_first_page_text} first page text, got {first_page_text}'
+    assert count == len(os.listdir(pdf_file_images_folder)), (f'Expected {count + 1} files in {pdf_file_images_folder},'
+                                                              f'got {len(os.listdir(pdf_file_images_folder))}')
