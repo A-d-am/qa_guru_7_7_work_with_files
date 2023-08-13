8b3eb8d38b155a7a58be1936b3d558d5c46a1c16
A-d-a-m <adam3928@mail.ru>
2023-08-13 15:33:40 +0200

    added assert

--- tests/test_xlsx.py
+++ tests/test_xlsx.py
@@ -1,8 +1,14 @@
 from openpyxl import load_workbook
 import os
+
+
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
 
 def test_xlsx():
     workbook = load_workbook('resources/file_example_XLSX_50.xlsx')
     sheet = workbook.active
-    print(sheet.cell(row=3, column=2).value)
+    row = 3
+    column = 2
+    value_in_given_cell = sheet.cell(row=row, column=column).value
+    assert value_in_given_cell == 'Mara', (f'Wrong value in cell {row = } {column = }: '
+                                           f'expected {"Mara"}, got {value_in_given_cell}')
