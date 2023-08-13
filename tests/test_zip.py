bb371a5b09abf53d517b09c7f64bee749484d2b3
A-d-a-m <adam3928@mail.ru>
2023-08-13 18:15:09 +0200

    added asserts

--- tests/test_zip.py
+++ tests/test_zip.py
@@ -1,8 +1,14 @@
 import zipfile
+import os
+from conftest import RESOURCES_DIR
 
+def test_zip():
+    zip_file_path = os.path.join(RESOURCES_DIR, 'hello.zip')
+    with zipfile.ZipFile(zip_file_path) as hellozip:
+        all_files_in_zip = hellozip.namelist()
+        hellozip.extract('hello/Hello.txt', path=RESOURCES_DIR)
+        text = hellozip.read('hello/Hello.txt')
+        assert 'hello/Hello.txt' in all_files_in_zip, f"No file {'Hello.txt'} in zip with name {'hello.zip'}"
+        assert text == b'Hello, World!'
 
-with zipfile.ZipFile('tests/resources/hello.zip') as hellozip:
-    hellozip.extract('Hello.txt')
-    print(hellozip.namelist())
-    text = hellozip.read('Hello.txt')
-    print(text)
+    os.remove(os.path.join(RESOURCES_DIR, 'hello'))
