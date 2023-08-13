594e7368c2beecb847a9dbe967baf42c16cabbd9
A-d-a-m <adam3928@mail.ru>
2023-08-13 15:25:20 +0200

    added test formalization

--- tests/test_xlsx.py
+++ tests/test_xlsx.py
@@ -1,6 +1,8 @@
 from openpyxl import load_workbook
+import os
 # TODO оформить в тест, добавить ассерты и использовать универсальный путь
 
-workbook = load_workbook('resources/file_example_XLSX_50.xlsx')
-sheet = workbook.active
-print(sheet.cell(row=3, column=2).value)
+def test_xlsx():
+    workbook = load_workbook('resources/file_example_XLSX_50.xlsx')
+    sheet = workbook.active
+    print(sheet.cell(row=3, column=2).value)
